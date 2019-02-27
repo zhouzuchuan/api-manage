1d7d4d3a761276a2823a600f8439e9340a5e5bad
fezzc <zhouzuchuan@icloud.com>
2019-02-27 17:28:27 +0800

    fix: bindApi 处理清单对象

--- src/bind-api.js
+++ src/bind-api.js
@@ -2,13 +2,13 @@ import { isFunction, isArray, isObject } from '../utils';
 
 export default (fns, params) => {
     return (isArray(fns) ? fns : [fns]).reduce((r, v) => {
-        if (!isFunction(v)) return r;
-        const result = v(params);
+        if (!(isFunction(v) || isObject(v))) return r;
+        const result = isFunction(v) ? v(params) : v;
         if (!isObject(result)) return r;
         return Object.entries(result).reduce((r2, [n, m]) => {
             return {
                 ...r2,
-                ...(isObject(m) && { [n]: { ...(r2[n] || {}), ...m } })
+                ...(isObject(m) && { [n]: { ...(r2[n] || {}), ...m } }),
             };
         }, r);
     }, {});
